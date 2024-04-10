a0f64db5ab9039b17c0a9b16ac5499c6e4bd4b0c
ShahriarCode <shahriarcode69@gmail.com>
2024-04-10 16:39:22 +0600

    project paused for some issues, will continue later

--- src/main.js
+++ src/main.js
@@ -1,1 +1,44 @@
 import '../styles/style.css'
+
+const addEventOnElements = function (element, eventType, callBack){
+  for(let i = 0, len = element.length; i < len; i++) {
+    element[i].addEventListener(eventType, callBack);
+  }
+}
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const togglerNavbar = function() {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-acive");
+}
+
+addEventOnElements(navTogglers, "click", togglerNavbar);
+
+
+const header = document.querySelector("[data-header]");
+
+let lastScrollPos = 0;
+
+const hideHeader = function(){
+  const isScrollBottom = lastScrollPos < window.scrollY;
+  if (isScrollBottom){
+    header.classList.add("hide");
+  }else {
+    header.classList.remove("hide")
+  }
+
+  lastScrollPos = window.scrollY
+}
+
+window.addEventListener("scroll", function(){
+  if (this.window.scrollY >= 50){
+    header.classList.add("active");
+    hideHeader();
+  } else{
+    header.classList.remove("active");
+  }
+})
